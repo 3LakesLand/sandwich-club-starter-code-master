8acf119e2258101f84cc0dd7d50f202b3b5a9094
Klaus Schlichting <kschlichting@gmx.ch>
2019-03-24 17:50:33 +0100

    added convenience method to convert List<String> in comma separated string

--- app/src/main/java/com/udacity/sandwichclub/model/Sandwich.java
+++ app/src/main/java/com/udacity/sandwichclub/model/Sandwich.java
@@ -2,7 +2,15 @@ package com.udacity.sandwichclub.model;
 
 import java.util.List;
 
+/**
+ *
+ */
 public class Sandwich {
+
+    private static final String SPACE = " ";
+    private static final String COMMA_AND_SPACE = ", ";
+    private static String NO_INFORMATION_AVAILABLE = "No Information available";
+
 
     private String mainName;
     private List<String> alsoKnownAs = null;
@@ -17,6 +25,15 @@ public class Sandwich {
     public Sandwich() {
     }
 
+    /**
+     *
+     * @param mainName
+     * @param alsoKnownAs
+     * @param placeOfOrigin
+     * @param description
+     * @param image
+     * @param ingredients
+     */
     public Sandwich(String mainName, List<String> alsoKnownAs, String placeOfOrigin, String description, String image, List<String> ingredients) {
         this.mainName = mainName;
         this.alsoKnownAs = alsoKnownAs;
@@ -30,47 +47,35 @@ public class Sandwich {
         return mainName;
     }
 
-    public void setMainName(String mainName) {
-        this.mainName = mainName;
-    }
-
-    public List<String> getAlsoKnownAs() {
-        return alsoKnownAs;
-    }
-
-    public void setAlsoKnownAs(List<String> alsoKnownAs) {
-        this.alsoKnownAs = alsoKnownAs;
+    public String getAlsoKnownAs() {
+        return getCommaSeparatedList(alsoKnownAs);
     }
 
     public String getPlaceOfOrigin() {
         return placeOfOrigin;
     }
 
-    public void setPlaceOfOrigin(String placeOfOrigin) {
-        this.placeOfOrigin = placeOfOrigin;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getImage() {
         return image;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public String getIngredients() {
+        return getCommaSeparatedList(ingredients);
     }
 
-    public List<String> getIngredients() {
-        return ingredients;
-    }
 
-    public void setIngredients(List<String> ingredients) {
-        this.ingredients = ingredients;
+    /**
+     * @param list
+     * @return
+     */
+    private String getCommaSeparatedList(List<String> list) {
+        if (list == null || list.isEmpty()) {
+            return NO_INFORMATION_AVAILABLE;
+        }
+        return String.join(COMMA_AND_SPACE, list);
     }
 }
