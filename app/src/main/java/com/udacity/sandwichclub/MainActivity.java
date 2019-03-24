ca2f9f617a5df863d49f18976ef99218fe41ea59
Klaus Schlichting <kschlichting@gmx.ch>
2019-03-24 19:57:23 +0100

    added Class Descriptions

--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -8,8 +8,18 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+/**
+ * The Class shows a List of Sandwiches to display. The Detailed View can be accessed
+ * via the selected Sandwich (Intent call).
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * The List of Sandwich Names is shown in the Surface.
+     *
+     * @param savedInstanceState If non-null, this Activity is being re-constructed
+     *                           from a previous saved state as given here
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, sandwiches);
 
-        // Simplification: Using a ListView instead of a RecyclerView
         ListView listView = findViewById(R.id.sandwiches_listview);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -30,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Call Of DetailActivity via Intent. The just clicked position from the Sandwich list
+     * is handed over to the Intent.
+     *
+     * @param position clicked position of the sandwich list
+     */
     private void launchDetailActivity(int position) {
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(DetailActivity.EXTRA_POSITION, position);
